70af5d4e462ddaf8b0c1f534c422100c523e94fb
uralozden <uralozden@gmail.com>
2020-07-19 14:50:48 +0300

    update for heroku

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // express'i dahil edelim,
 const app = require('express')()
 const TikTokScraper = require('tiktok-scraper');
+
 var Port = process.env.Port || 3000
 // HTTP GET isteğine JSON tipinde yanıt verelim,
 app.get('/', (req, res) => {
 
   (async () => {
     try {
-        const posts = await TikTokScraper.hashtag('kombi', { number: 500 });
+        const posts = await TikTokScraper.hashtag('petrolofisi', { number: 10 });
+        if(!posts){
+          posts = "bir sorun var"
+        }
         res.json(posts)
 
     } catch (error) {
